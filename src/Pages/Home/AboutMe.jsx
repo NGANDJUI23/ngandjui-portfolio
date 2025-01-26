1e4287cb2073aaa022d0a3b23780c7f839722df5
ngandjui <ngandjui.ivan@institutsaintjean.org>
2025-01-26 15:17:12 +0100

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">Who am I ?</h1>
+          <p className="hero--section-description">
+            My name is NGANDJUI HANGA Ivan Delors, an engineering student at Saint Jean Engineering, level 4, majoring in Computer Science and Information Systems.
+            During this journey, I have been able to acquire skills in SI modeling, project management, web development, technician work, database administration, basic enterprise network administration/design/configuration, and I have recently been certified as a full-stack developer by the Code Academy of the National Advanced School of Engineering in Yaoundé, demonstrating my high competencies in Python, Data Analysis, Java, the J2EE framework, deployment, web design (HTML - CSS - JS), Bootstrap, Angular, and ReactJS.
+            Ngandjui Ivan is also very responsive and effective at finding solutions to situations due to the skills he has acquired in Algorithms
+          </p>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+            recusandae?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Ivan Ngandjui</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Software Ingenieer</span>{" "}
+            <br />
+            Fullstack Developer
+          </h1>
+          <p className="hero--section-description">
+          My name is NGANDJUI HANGA Ivan Delors, an engineering student at Saint Jean Engineering, level 4, majoring in Computer Science and Information Systems.
+            <br /> Wellcome to my portfolio
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.jpeg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
