f3b75c5c4ddb8aec51e421ae6efd134b8fb58484
ngandjui <ngandjui.ivan@institutsaintjean.org>
2025-02-10 15:23:27 +0100

    Update

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,4 +1,32 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 export default function ContactMe() {
+    const [email, setEmail] = useState('');
+    const [first_name, setFirstname] = useState('');
+    const [last_name, setLastname] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const [phone_number, setPhonenumber] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        alert("xend");
+        const templateParams = {
+            from_name: email,
+            subject: subject,
+            message: message,
+        };
+
+        emailjs.send('service_z5qy1p9', 'template_h4iyhkv', templateParams, 'j0jzkJxbKi9-ujAM-VF78')
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                setResponseMessage('Email envoyé avec succès!');
+            }, (err) => {
+                console.error('FAILED...', err);
+                setResponseMessage('Échec de l\'envoi de l\'email.');
+            });
+    };
   return (
     <section id="Contact" className="contact--section">
       <div>
@@ -8,7 +36,7 @@ export default function ContactMe() {
           Ecrivez-moi pour un contrat, une question, ou pour ameliorer ma page
         </p>
       </div>
-      <form className="contact--form--container">
+      <form className="contact--form--container" onsubmit={sendEmail}>
         <div className="container">
           <label htmlFor="first-name" className="contact--label">
             <span className="text-md">First Name</span>
@@ -17,6 +45,8 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="first-name"
               id="first-name"
+              value={first_name}
+              onChange={(e) => setFirstname(e.target.value)}
               required
             />
           </label>
@@ -27,6 +57,8 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="last-name"
               id="last-name"
+              value={last_name}
+              onChange={(e) => setLastname(e.target.value)}
               required
             />
           </label>
@@ -37,6 +69,8 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </label>
@@ -47,25 +81,20 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="phone-number"
               id="phone-number"
+              value={phone_number}
+              onChange={(e) => setPhonenumber(e.target.value)}
               required
             />
           </label>
         </div>
-        <label htmlFor="choode-topic" className="contact--label">
-          <span className="text-md">Choose a topic</span>
-          <select id="choose-topic" className="contact--input text-md">
-            <option>Select One...</option>
-            <option>Item 1</option>
-            <option>Item 2</option>
-            <option>Item 3</option>
-          </select>
-        </label>
         <label htmlFor="message" className="contact--label">
           <span className="text-md">Message</span>
           <textarea
             className="contact--input text-md"
             id="message"
             rows="8"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             placeholder="Type your message..."
           />
         </label>
@@ -74,9 +103,10 @@ export default function ContactMe() {
           <span className="text-sm">I accept the terms</span>
         </label>
         <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
+          <input type='submit' className="btn btn-primary contact--form--btn" value="submite"/>
         </div>
       </form>
+      {responseMessage && <p>{responseMessage}</p>}
     </section>
   );
 }
